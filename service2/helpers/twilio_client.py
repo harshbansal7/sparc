96a76520a6fd6aafd9f15830e2d943746164d57b
harshbansal7 <harshbansal1036@gmail.com>
2024-03-31 01:00:33 +0530

    Added Whatsapp Reply for updating Complaint Status

--- service2/helpers/twilio_client.py
+++ service2/helpers/twilio_client.py
@@ -8,14 +8,14 @@ sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth = os.getenv("TWILIO_AUTH_TOKEN")
 twilio_client = Client(sid, auth)
 
-def dispatch_message_to_whatsapp(packet):
+def dispatch_message_to_whatsapp(msg, contactno):
     message = twilio_client.messages.create(
         from_='whatsapp:+14155238886',
-        body=packet[0],
-        to=f"whatsapp:{packet[1]}"
+        body=msg,
+        to=f"whatsapp:{contactno}"
     )
 
     if not message.error_code:
-        return True
+        return str(message.sid)
     
     return False
